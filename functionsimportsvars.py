b8b3495d9243f349e8876fbe289a6504ce9749cf
Your Name <you@example.com>
2023-09-09 17:12:59 -0400

    redshit added

--- functionsimportsvars.py
+++ functionsimportsvars.py
@@ -6,6 +6,15 @@ from scipy.integrate import simpson
 from numpy import trapz
 import csv
 
+altitude_angle_observer = 45
+azimuth_angle_observer = 180
+distance_from_center_to_observer_ly = 100
+num_of_rings = 100
+num_of_slices = 16
+distance_from_center_to_observe_cm = distance_from_center_to_observer_ly * (9.46 * (10**17))
+altitdue_angle_observer_radians = altitude_angle_observer * (math.pi/180)
+azimuth_angle_observer_radians = azimuth_angle_observer * (math.pi/180)
+
 #Defining the mass of black hole in solar masses and grams
 bh_solar_mass = 4100000 #This is for the Sigitarus A* which is in the center of the Milky Way
 bh_grams = bh_solar_mass * 1.989 * (10 ** 33)
@@ -36,4 +45,21 @@ def find_spectral_radiance(temperature, frequency):
         second_part = 1/((math.e ** ((plank_h * frequency)/(k_b * temperature))) - 1)
         return first_part * second_part
 
-num_of_rings = 100
+def find_keplerian_velocity(radius, mass):
+        return math.sqrt((G * mass)/radius)
+
+def get_lorentz_factor(velocity):
+        return 1/(math.sqrt(1-(velocity**2/c**2)))
+
+def get_azimuth_angle_diff(angle):
+        azimuth_angle_diff = abs(angle - azimuth_angle_observer)
+        return azimuth_angle_diff
+
+def optimal_angle_of_velocity(radius, d_observer):
+        return azimuth_angle_observer_radians + math.acos(radius/d_observer)
+
+def get_velocity_vector(angle):
+        return angle + 90
+
+def get_redshift(velocity, frequency, optimal_angle):
+        return (frequency)/(get_lorentz_factor(velocity) * (1 + (velocity/c) * math.cos(optimal_angle)))
--- radintensityvsfrequency.py
+++ radintensityvsfrequency.py
@@ -2,22 +2,37 @@ from functionsimportsvars import *
 import random
 
 
-observer_angle = 90
-angle_in_radians = math.radians(observer_angle)
-print(math.cos(angle_in_radians))
-# Creating the x and y values of the plot
-x_vals = np.array([i * (10 ** 13) for i in np.arange(10, 10**4, .1)])
 
+
+
+width_ring = ((outer_acretion_disk - inner_acretion_disk)//radius_schwarz + 1)/num_of_rings
 """file = open("tempvsspectra", "w")
 writer = csv.writer(file)
 top_row = ["Ring Number ", "Temperature ", "Frequecny ", "Spectral Radiance ", "Flux"]
-writer.writerow(top_row)"""
+writer.writerow(top_row)
+xval = 0
+yval = 0
+fig, axs = plt.subplots(4, 3)
+for i in range(1, 13):
+        while yval < 3:
+                if xval < 4:
+                        [xval, yval].plot()"""
+x_vals = []
+y_vals = []
+for i in np.arange(10, 10**4, 10**3):
+    frequency = i * (10 ** 13)           
+    for t in range(1, num_of_rings + 1):
+        distance_from_ring = 3 * radius_schwarz + width_ring * (t) * radius_schwarz
+        for r in range(0, num_of_slices):
+                observer_angle = 360/num_of_slices
+                angle_in_radians = math.radians(observer_angle)
+                x_vals.append(frequency)
+                y_vals.append([find_spectral_radiance(find_temp_of_acretion((.25 * radius_schwarz) + 3 * radius_schwarz), frequency)])
+                x_vals = []
+                y_vals = []
+                plt.plot(x_vals, get_redshift(find_keplerian_velocity(distance_from_ring, bh_grams), frequency, optimal_angle_of_velocity(distance_from_ring, distance_from_center_to_observe_cm)) * math.cos(angle_in_radians) * np.array(y_vals))
 
-for r in range(1, 2):
-        y_vals = np.array([find_spectral_radiance(find_temp_of_acretion(r * (.25 * radius_schwarz) + 3 * radius_schwarz), i * (10 ** 13)) for i in np.arange(10, 10**4, .1)])
-        plt.plot(x_vals, math.cos(angle_in_radians) * y_vals)
-        print(math.cos(angle_in_radians) * x_vals)
-        print(math.cos(angle_in_radians) * y_vals)
+
 #Using the trapz function to find the area under the curve which is the flux
 y_vals2 = [find_spectral_radiance(find_temp_of_acretion(50 * (.25 * radius_schwarz) + 3 * radius_schwarz), i * (10 ** 13)) for i in np.arange(10, 10**4, .1)]
 y = np.array(y_vals2)
@@ -26,6 +41,8 @@ print(flux)
 
 #Plotting the grpah
 plt.xscale("log")
+plt.yscale("log")
 plt.xlabel("Frequency(hertz)")
 plt.ylabel("Spectral Radiance Intensity(Bv(t))")
+plt.legend()
 plt.show()
